a9e07692e8f429dff29aef04efa4d21d2727497a
Chango <dspm0980711658@gmail.com>
2023-10-05 13:51:34 +0800

    02

--- index.py
+++ index.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.jinja.html")
+@app.route("/feature")
+def feature():
+    return render_template("feature.jinja.html")
