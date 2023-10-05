5939380febab91ad34b2d3937caf6a562aed0232
Chango <dspm0980711658@gmail.com>
2023-10-05 15:02:24 +0800

    25

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  
 
 app = Flask(__name__)
 
@@ -8,3 +8,9 @@ def index():
 @app.route("/feature")
 def feature():
     return render_template("feature.jinja.html")
+@app.route("/description")
+def description():
+    return render_template("description.jinja.html")
+@app.route("/product")
+def product():
+    return render_template("product.jinja.html")
